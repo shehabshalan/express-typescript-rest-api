89905c70bab0549d9e281ddac23cb33526d56684
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-17 21:49:21 +0200

    user schema

---
+++ src/models/user.model.ts
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import config from "config";
+
+export interface IUser extends mongoose.Document {
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  let user = this as IUser;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+  const hash = await bcrypt.hashSync(user.password, salt);
+  user.password = hash;
+  return next();
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  /// because it's async, we need to return a promise of a type in this case boolean
+  const user = this as IUser;
+  const isMatch = await bcrypt
+    .compare(candidatePassword, user.password)
+    .catch(() => false);
+  return isMatch;
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
