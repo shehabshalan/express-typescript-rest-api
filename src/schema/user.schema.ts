02a2cac3203a88b4ae264b3c1e2d4ffde85cf54b
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-20 22:48:16 +0200

    user schema added

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,23 @@
+import { object, string, TypeOf } from "zod";
+
+export const createUserSchema = object({
+  body: object({
+    name: string({
+      required_error: "Name is required",
+    }),
+    password: string({
+      required_error: "Password is required",
+    }).min(6, "Password must be at least 6 characters"),
+    passwordConfirmation: string({
+      required_error: "Password confirmation is required",
+    }),
+    email: string({
+      required_error: "Email is required",
+    }).email("Not a valid email address"),
+  }).refine((data) => data.password === data.passwordConfirmation, {
+    message: "Password and password confirmation must match",
+    path: ["passwordConfirmation"],
+  }),
+});
+
+export type CreateUserInput = TypeOf<typeof createUserSchema>;
