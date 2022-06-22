5cb26a969b7953214a78b812a363b1a4737703f8
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-22 23:01:59 +0200

    omited mongo properties

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,7 +1,11 @@
 import { DocumentDefinition } from "mongoose";
 import UserModel, { IUser } from "../models/user.model";
 
-export async function createUser(input: DocumentDefinition<IUser>) {
+export async function createUser(
+  input: DocumentDefinition<
+    Omit<IUser, "createdAt" | "updatedAt" | "comparePassword">
+  >
+) {
   try {
     return await UserModel.create(input);
   } catch (e: any) {
